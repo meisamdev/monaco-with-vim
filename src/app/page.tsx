2e39fa70e5ed5d21a7da843196bb7b7adb3f9311
Meysam Mahmoudi <meisamdev@gmail.com>
2024-12-08 11:53:18 +0330

    Add vim support to monaco editor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,58 @@
+"use client"
 import Image from "next/image";
+import React, { useState } from 'react';
+import Editor from '@monaco-editor/react';
+import { editor } from 'monaco-editor';
+
 
 export default function Home() {
+  const [formData, setFormData] = useState({
+    isVimEnabled: true
+  });
+
+  function handleEditorDidMount(editor: editor.IStandaloneCodeEditor) {
+    // setup monaco-vim
+    // @ts-expect-error fix non-TS code
+    window.require.config({
+      paths: {
+        "monaco-vim": "https://unpkg.com/monaco-vim/dist/monaco-vim"
+      }
+    });
+
+    // @ts-expect-error fix non-TS code
+    window.require(["monaco-vim"], function (MonacoVim) {
+      const statusNode = document.querySelector(".status-node");
+      MonacoVim.initVimMode(editor, statusNode);
+    });
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+
+        {formData.isVimEnabled
+          ?
+          <>
+            <Editor
+              height="60vh"
+              width="50vw"
+              language="javascript"
+              onMount={handleEditorDidMount}
+              defaultValue="// text code"
+              theme="vs-dark"
+            />
+            <code className="status-node"></code>
+          </>
+          :
+          <Editor
+            height="60vh"
+            width="50vw"
+            language="javascript"
+            defaultValue="// text code"
+            theme="vs-dark"
+          />
+        }
+
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
@@ -24,29 +65,13 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
+          <button
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
+            onClick={() => setFormData({ isVimEnabled: !formData.isVimEnabled })}
             rel="noopener noreferrer"
           >
-            Read our docs
-          </a>
+            {formData.isVimEnabled ? "Disable vim" : "Enable vim"}
+          </button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
